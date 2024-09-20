d474989fbeb361461b78def36e70f99f9f34b664
Keith Flynn <flynn.keith@gmail.com>
2024-09-19 21:52:22 -0400

    Add BikeInventory and Unicycles

---
+++ sdc1-knowledge-check-2/Unicycles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdc1_knowledge_check_2
+{
+    internal class Unicycles : BikeInventory
+    {
+        public string RideStyle { get; set; } = "";
+        public bool IsGiraffe { get; set; }
+    }
+}
